ea332807675d4de857cd3030b4a91d15c3c55d36
Sardor <isardor0707@gmail.com>
2024-04-19 18:03:47 +0300

    some docstrings added

--- solver.py
+++ solver.py
@@ -19,8 +19,8 @@ class Source:
         '''
         :param z: redshift of Source
         :param center: center of the Source in angle plane (in arcseconds)
-        :param direction: the angle of the Source radiation (in degrees)
-        :param angle: the rotation angle of the Source (in degrees)
+        :param direction: angle between the jet direction and the observer view direction (in degrees)
+        :param angle: rotation angle measured from the vertical in clockwise arrow (in degrees)
         '''
 
         self.z = z
@@ -34,6 +34,18 @@ class Source:
                  else self.createLineSource(length, direction, angle, num)
 
     def createLineSource(self, length, direction, angle, num):
+        '''
+        creates the Source in shape of line .
+
+        output format [[x1, y1, d1], [x2, y2, d2], ... ], \
+        where xi, yi are scaled into angles in arcseconds, di - angular diameter distance in kpc.
+
+        :param length: length of the source i.e. cylinder height (in kpc)
+        :param direction: angle between the jet direction and the observer view direction (in degrees)
+        :param angle: rotation angle measured from the vertical in clockwise arrow (in degrees)
+        :param num: amount of points in the Source
+        :return: an array of Source points
+        '''
         v = np.linspace([0, 0, 0], [0, 0, -length], num)
         v = self.rotateX(v, direction)
         v = self.rotateZ(v, angle)
@@ -41,7 +53,18 @@ class Source:
         return self.scale(v)
 
     def createCircleSource(self, radius, num):
+        '''
+        creates the Source in shape of circle.
+
+        output format [[x1, y1, d1], [x2, y2, d2], ... ], \
+        where xi, yi are scaled into angles in arcseconds, di - angular diameter distance in kpc.
+
+        :param radius: radius of the circle (in kpc)
+        :param num: amount of points in the Source
+        :return: an array of Source points
+        '''
         theta = np.linspace(0, 2 * np.pi, num)
+        radius = self.D_s * radius / arcsec
         x = radius * np.cos(theta)
         y = radius * np.sin(theta)
 
@@ -50,6 +73,19 @@ class Source:
         return self.scale(np.array([x, y, d]).T)
 
     def createCylinderSource(self, length, radius, direction, angle, num):
+        '''
+        creates the Source in shape of cylinder.
+
+        output format [[x1, y1, d1], [x2, y2, d2], ... ], \
+        where xi, yi are scaled into angles in arcseconds, di - angular diameter distance in kpc.
+
+        :param length: length of the source i.e. cylinder height (in kpc)
+        :param radius: radius of the source (in kpc)
+        :param direction: angle between the jet direction and the observer view direction (in degrees)
+        :param angle: rotation angle measured from the vertical in clockwise arrow (in degrees)
+        :param num:  amount of points in the Source
+        :return: an array of Source points
+        '''
         # z = np.linspace(0, length, round(num ** (1 / 3) / 9))
         # theta = np.linspace(0, 2 * np.pi, round(num ** (1 / 3)) * 9)
         # radii = np.linspace(0, radius, round(num ** (1 / 3)))
@@ -91,6 +127,11 @@ class Source:
         return list(map(lambda p: A @ p, points))
 
     def scale(self, points):
+        '''
+        scales the coordinates from kpc into angles and moves the center of the Source to the given point
+        :param points: an array of points
+        :return:
+        '''
         s = np.array(points).T
         s[0:2] *= arcsec / self.D_s
         s = s.T
@@ -100,22 +141,41 @@ class Source:
 
 
 class Solver:
-    def __init__(self, lens, source) -> None:
+    def __init__(self, lens: Lens, source: Source) -> None:
+        '''
+        :param lens: an object of the Lens class. the lens of the system
+        :param source: an object of the Source class. the source of the system
+        '''
         self.lens = lens
         self.source = source
 
     def einsteinRadius(self, D_s):
+        '''
+        the Einstein radius for point with angular diameter distance equal to z.
+        :param D_s: an angular diameter distance of point
+        :return: the Einstein radius in degrees
+        '''
         D_ls = self.lens.D_ls
         D_l = D_s - D_ls
 
         return np.sqrt(4 * constants.G.value * self.lens.m / constants.c.value ** 2 * D_ls / D_l / D_s / 3e19) * arcsec
 
     def einsteinRadiusZ(self, z):
+        '''
+        the Einstein radius for point with redshift equal to z.
+        :param z: redshift of point
+        :return: the Einstein radius in degrees
+        '''
         D_s = model.angular_diameter_distance(z)
 
         return self.einsteinRadius(D_s)
 
     def processPoint(self, p_):
+        '''
+        processes the images of a single point.
+        :param p_: the point to be lensed. format [x1, y1, d1]
+        :return: the coordinates of two images in format [im1, im2]
+        '''
         p = np.matrix(p_[0:2])
         D_s = p_[2]
         if self.source.D_s - D_s > self.lens.D_ls:
@@ -135,6 +195,10 @@ class Solver:
         return np.array(points)
 
     def processImage(self):
+        '''
+        processes a whole image of the source
+        :return: image coordinates of each point in source. format [[im1_1, im1_2], [im2_1, im2_2], ... ]
+        '''
         points = list(map(lambda p: self.processPoint(p), self.source.points))
 
         return np.transpose([pp for p in points for pp in p])
